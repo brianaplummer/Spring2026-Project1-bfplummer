64a421cfa53ae7ed0fccf5ab6f49e12c723c47c4
brianaplummer <bfplummer@crimson.ua.edu>
2026-02-05 15:07:24 -0600

    committing files

---
+++ Spring2026-Project1-bfplummer/Spring2026-Project1-bfplummer/wwwroot/js/quote.js
@@ -0,0 +1,21 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+const button = document.getElementById("quoteButton");
+const quoteText = document.getElementById("quoteText");
+
+const quotes = [
+  "Bears. Beets. Battlestar Galactica.",
+  "I declare bankruptcy!",
+  "Identity theft is not a joke, Jim!",
+  "Did I stutter?",
+  "I am Beyoncé, always."
+];
+
+button.addEventListener("click", () => {
+  quoteText.classList.remove("hidden");
+  const randomIndex = Math.floor(Math.random() * quotes.length);
+  quoteText.textContent = quotes[randomIndex];
+});
